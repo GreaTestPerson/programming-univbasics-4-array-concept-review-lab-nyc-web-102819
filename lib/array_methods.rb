9136ffafa57b690043fc3e164978627f25f0be38
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-16 04:31:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,13 +5,18 @@
   zig_zag = [500,4,1000,5,250]
   
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter= 0
+    while counter < array.length do
+    if array[counter] == value_to_find
+      return counter 
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+
 end
 
 def find_min_value(array)
-  # Add your solution here
+
 end
+
