35b74d832fc86df73b5771be468432f00734caae
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-16 04:34:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,15 +10,9 @@ def find_element_index(array, value_to_find)
     if array[counter] == value_to_find
       return counter 
   end
+   counter+= 1
 end
 
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1
-  end
-  nil
 
 def find_max_value(array)
 
