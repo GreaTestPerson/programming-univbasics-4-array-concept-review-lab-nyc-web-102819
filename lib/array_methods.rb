9af506a2c357fd883478269bf94ffa1e4623c556
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-16 04:35:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,6 +19,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-
+  number = array[0]
+    array.length.times do |index|
+      if array[index] < number
+        number = array[index]
+      end
+  end
+  number
 end
 
